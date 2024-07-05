c7c4e115deaa70cc2e6c75dc4c59e23898370502
Brenda Marmolejo <bmarmolejo@gmail.com>
2024-07-05 09:21:33 -0400

    added dashboard page and path in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.scss";
 import Points from "./pages/Points/Points";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WorkoutPage from "./pages/WorkoutPage/WorkoutPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/points" element={<Points />} />
           <Route path="/workout" element={<WorkoutPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
         </Routes>
       </BrowserRouter>
     </>

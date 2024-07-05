dffac514cb62b23ad4d213ae43c7761f3ef4432e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-07-05 07:51:00 -0700

    add: links to homepage on points and prize page

--- src/pages/Points/Points.jsx
+++ src/pages/Points/Points.jsx
@@ -3,6 +3,7 @@ import backArrow from "../../assets/icons/back-arrow.png";
 import teamLogo from "../../assets/images/team-logo.png";
 import Lottie from "react-lottie";
 import animationData from "../../assets/lotties/confetti.json";
+import { useNavigate } from "react-router-dom";
 const Points = () => {
   const defaultOptions = {
     loop: false,
@@ -12,16 +13,28 @@ const Points = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="points">
       <div className="points__nav">
-        <img className="points__back-icon" src={backArrow} />
+        <div onclick={handleHome}>
+          <img
+            className="points__back-icon"
+            src={backArrow}
+            alt="returning icon"
+          />
+        </div>
         <p className="points__nav-title">The Rank</p>
       </div>
       <div className="points__body">
         <h1 className="points__title">Congratulations</h1>
         <div className="points__point-container">
           <p className="points__total-comment">Your total points</p>
+          {/* need to add dynamic data for points */}
           <p className="points__total">10,050 pts</p>
         </div>
         <div className="points__team-container">
@@ -29,10 +42,13 @@ const Points = () => {
           <img className="points__logo" src={teamLogo} />
         </div>
         <div className="points__points">
+          {/* need to add dynamic data for points */}
           <p className="points__earned">100 pts</p>
         </div>
       </div>
-      <footer className="points__footer"> GO BACK</footer>
+      <footer className="points__footer" onClick={handleHome}>
+        <button className="points__button-back"> GO BACK</button>
+      </footer>
     </div>
   );
 };
--- src/pages/PrizePage/PrizePage.jsx
+++ src/pages/PrizePage/PrizePage.jsx
@@ -1,52 +1,70 @@
 import "./PrizePage.scss";
 import backArrow from "../../assets/icons/back-arrow.png";
 import teamLogo from "../../assets/images/team-logo.png";
-import ticketIcon from "../../assets/icons/ticket.png"
+import ticketIcon from "../../assets/icons/ticket.png";
 import Lottie from "react-lottie";
 import animationData from "../../assets/lotties/confetti.json";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function PrizePage() {
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-    const defaultOptions = {
-        loop: false,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice",
-        },
-    };
+  const navigate = useNavigate();
+  const handleHome = () => {
+    navigate("/");
+  };
 
-    return (
-        <>
-            <div className="prize">
-                <div className="prize__nav">
-                    <img  className="prize__nav--icon" src={backArrow} alt="returning icon"/>
-                    <h3 className="prize__nav--title" >The Rank</h3>
-                </div>
-                <div className="prize__body">
-                    <h1 className="prize__title">Congratulations</h1>
-                    <div className="prize__team-container">
-                        <Lottie options={defaultOptions} height={600} width={900} />
-                        <img className="prize__logo" src={teamLogo} />
-                    </div>
-                    <div className="prize__result">
-                        <p className="prize__result--text">YOU WON</p>
-                    </div>
-                </div>
-                <div>
-                    <div className="prize__rank">
-                        <p className="prize__rank--place">Your team was 4th place</p>
-                        <h2 className="prize__rank--entries">You got 200 entries</h2>
-                    </div>
-                    <div className="prize__ticket">
-                        <p className="prize__ticket--title">Ticket to a game</p>
-                        <img src={ticketIcon} alt="icon for ticket" className="prize__ticket--icon"/>
-                        <p>Go to your account to claim</p>
-                    </div>
-                </div> 
-            <button className="prize__footer"> GO BACK</button>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="prize">
+        <div className="prize__nav">
+          <div>
+            <img
+              className="prize__nav--icon"
+              src={backArrow}
+              alt="returning icon"
+              onClick={handleHome}
+            />
+          </div>
+          <h3 className="prize__nav--title">The Rank</h3>
+        </div>
+        <div className="prize__body">
+          <h1 className="prize__title">Congratulations</h1>
+          <div className="prize__team-container">
+            <Lottie options={defaultOptions} height={600} width={900} />
+            <img className="prize__logo" src={teamLogo} />
+          </div>
+          <div className="prize__result">
+            <p className="prize__result--text">YOU WON</p>
+          </div>
+        </div>
+        <div>
+          <div className="prize__rank">
+            <p className="prize__rank--place">Your team was 4th place</p>
+            <h2 className="prize__rank--entries">You got 200 entries</h2>
+          </div>
+          <div className="prize__ticket">
+            <p className="prize__ticket--title">Ticket to a game</p>
+            <img
+              src={ticketIcon}
+              alt="icon for ticket"
+              className="prize__ticket--icon"
+            />
+            <p>Go to your account to claim</p>
+          </div>
+        </div>
+        <button className="prize__footer" onClick={handleHome}>
+          {" "}
+          GO BACK
+        </button>
+      </div>
+    </>
+  );
 }

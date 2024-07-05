136f8fa93611a45b3d972a03fa32a98c7d4a9e92
maishamir <45680002+maishamir@users.noreply.github.com>
2024-07-05 10:01:22 -0700

    fixed styling; added confetti animation on workout complete

--- src/pages/WorkoutPage/WorkoutPage.jsx
+++ src/pages/WorkoutPage/WorkoutPage.jsx
@@ -3,23 +3,45 @@ import videoPoster from "../../assets/images/VideoPoster.jpg";
 import circleIcon from "../../assets/icons/circle.svg";
 import circleCheck from "../../assets/icons/circleCheck.svg";
 import { useState } from "react";
+import workoutCompleteAnimation from "../../assets/lotties/workout-complete.json";
+import Lottie from "react-lottie";
 
 function WorkoutPage() {
   const [buttonText, setButtonText] = useState("Mark Complete");
-    const [iconImg, setIconImg] = useState(circleIcon);
-    const [showElement, setShowElement] = useState(true)
+  const [iconImg, setIconImg] = useState(circleIcon);
+  const [showElement, setShowElement] = useState(true);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [styles, setStyles] = useState({});
 
-    function handlePlay() {
-        setShowElement(false)
-    }
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: workoutCompleteAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  function handlePlay() {
+    setShowElement(false);
+  }
 
   function handleClick() {
     if (buttonText === "Workout Complete" && iconImg === circleCheck) {
       setButtonText("Mark Complete");
       setIconImg(circleIcon);
+      setTimeout(() => setShowConfetti(false), 2500);
+        setShowConfetti(false);
+        setStyles(null)
     } else {
       setButtonText("Workout Complete");
       setIconImg(circleCheck);
+      setShowConfetti(true);
+      let styleValues = {
+        "background":
+          "linear-gradient(100deg, hsla(214, 100%, 50%, 1) 0%, hsla(186, 97%, 53%, 1) 100%)",
+      };
+        setStyles(styleValues)
     }
   }
   return (
@@ -34,7 +56,8 @@ function WorkoutPage() {
 
         <div className="workout-page__video-container">
           {showElement && <p className="workout-page__level">EASY</p>}
-          <video onPlay={handlePlay}
+          <video
+            onPlay={handlePlay}
             controls
             poster={videoPoster}
             className="workout-page__video"
@@ -42,16 +65,28 @@ function WorkoutPage() {
         </div>
 
         <div className="workout-page__video-info">
-          <div class="workout-page__tags">
-            <span className="workout-page__tag">Group</span>
-            <span className="workout-page__tag">Sport</span>
+          <div className="workout-page__tags">
+            <span className="workout-page__tag">Upper Body</span>
+            <span className="workout-page__tag">Basketball</span>
           </div>
 
           <button
+            style={ styles }
             className="workout-page__complete"
             type="button"
             onClick={handleClick}
           >
+            {showConfetti && (
+              <div className="workout-page__confetti">
+                <Lottie
+                  options={defaultOptions}
+                  height={400}
+                  width={400}
+                  isStopped={!showConfetti}
+                  isPaused={false}
+                />
+              </div>
+            )}
             {buttonText}
             <span>
               <img src={iconImg} alt="" className="workout-page__icon" />

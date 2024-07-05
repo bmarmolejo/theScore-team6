c5b1e07e8d22826473c2cde36704344eab1c69b0
Denise Deng <siktang1221@gmail.com>
2024-07-05 08:23:30 -0400

    created structure

--- src/pages/PrizePage/PrizePage.jsx
+++ src/pages/PrizePage/PrizePage.jsx
@@ -3,6 +3,12 @@ import "./PrizePage.scss";
 export default function PrizePage() {
 
     return (
-        <main></main>
+        <main>
+            <h3>The Rank</h3>
+            <h1>Congratulations!</h1>
+            <img alt="raptors logo"/>
+            <p>YOU WON</p>
+            
+        </main>
     )
 }
